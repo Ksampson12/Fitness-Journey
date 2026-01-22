6bd537065fca2b5402acc458ee8e189046ebebd9
ksampson12 <ksampson1218@gmail.com>
2026-01-22 17:00:20 -0500

    removed JWT verif

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installing dependencies...
+npm install
+echo Dependencies installed!
+echo Now run: npm run dev
+pause
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing dependencies..."
+npm install
+echo "Dependencies installed!"
+echo "Now run: npm run dev"
